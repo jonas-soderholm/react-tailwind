5a80f37d43d297c1378ad1efd67dce6f93c01bec
Jonas <jonas.soderholm89@gmail.com>
2023-11-29 12:11:26 +0100

    Add navbar and hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,40 @@
 function Hero() {
   return (
-    <div className="text-center p-10 bg-slate-900 max-w-2xl mx-auto">
-      <h2 className="text-5xl text-cyan-500">Jonas Söderholm</h2>
-      <h2 className="text-2xl text-black-500 pt-2">Developer and Designer</h2>
-      <p className="text-md pt-5">
-        Aasdasda asd asd a sd asa sdas d asdasdasdasd a sdas d d asa sdas d
-        asdasdasdasd a sdas d d asa sdas d asdasdasdasd a sdas dd asa sdas d
-        asdasdasdasd a sdas d
-      </p>
+    <div className="main-container bg-gray-900">
+      <div className="mx-4 bg-gray-900 text-white rounded-xl border border-white h-screen flex flex-col justify-center">
+        <div className="w-full overflow-x-hidden"></div>
+      </div>
     </div>
   );
 }
 
 export default Hero;
+
+//     <div className="p-1 bg-slate-900 flex flex-wrap-reverse justify-between gap-2">
+//       <div className="p-10 flex-1 bg-red-500">
+//         <h2 style={{ fontSize: "5vh" }} className="text-cyan-500">
+//           Jonas Söderholm
+//         </h2>
+//         <h2 style={{ fontSize: "4vh" }} className="text-black-500 text-white">
+//           Developer and Designer
+//         </h2>
+//         <p className="pt-2 text-white">
+//           Aasdasda asd asd a sd asa sdas d asdasdasdasd a sdas d d asa sdas d
+//           asdasdasdasd a sdas d d asa sdas d asdasdasdasd a sdas dd asa sdas d
+//           asdasdasdasd a sdas d
+//         </p>
+//       </div>
+//       <div className="p-10 flex-1  bg-blue-500">
+//         <h2 style={{ fontSize: "5vh" }} className="text-cyan-500">
+//           Jonas Söderholm
+//         </h2>
+//         <h2 style={{ fontSize: "4vh" }} className="text-black-500 text-white">
+//           Developer and Designer
+//         </h2>
+//         <p className=" pt-2 text-white">
+//           Aasdasda asd asd a sd asa sdas d asdasdasdasd a sdas d d asa sdas d
+//           asdasdasdasd a sdas d d asa sdas d asdasdasdasd a sdas dd asa sdas d
+//           asdasdasdasd a sdas d
+//         </p>
+//       </div>
+//     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsFillMoonStarsFill, BsList } from "react-icons/bs";
 
 function Navbar() {
   const [count, setCount] = useState(0);
@@ -8,21 +8,30 @@ function Navbar() {
     <>
       <div
         className="Navbar flex justify-between items-center p-5
-        bg-slate-900 text-white"
+        bg-gray-900 text-white"
       >
-        <h1 className="text-4xl">H</h1>
+        <div className="flex items-center">
+          <img src="/sweden-flag.png" alt="" className="w-11 h-auto" />
+          {/* <div className="ml-2">
+            <h2 style={{ fontSize: "10px" }}>Stockholm</h2>
+            <h2 style={{ fontSize: "10px" }}>Sweden</h2>
+          </div> */}
+        </div>
         <div className="nav-links">
           <ul className="flex gap-4 text-1xl items-center">
-            <li>
+            <li className="">
               <BsFillMoonStarsFill />
             </li>
             <li>
-              <button className="git-btn bg-transparent text-white p-1.5 ml-3 rounded-lg border-2 border-cyan-500 ">
+              <button className="git-btn bg-transparent text-white p-1.5 ml-3 rounded-lg border-2 border-white ">
                 Resume
               </button>
-              <button className="git-btn  bg-cyan-500 text-white p-2 ml-3 rounded-lg ">
+              <button className="git-btn  bg-white text-black p-2 ml-3 rounded-lg ">
                 Git repos
               </button>
+            </li>
+            <li>
+              <BsList style={{ fontSize: "35px" }} />
             </li>
           </ul>
         </div>
