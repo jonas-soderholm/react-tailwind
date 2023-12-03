da1fe2d799eeaa212b4f486499c00bb721031443
Jonas <jonas.soderholm89@gmail.com>
2023-12-03 06:17:59 +0100

    Add navbar dropdown with animations

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,40 +1,132 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BsFillMoonStarsFill, BsList } from "react-icons/bs";
 
 function Navbar() {
-  const [count, setCount] = useState(0);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [IsBurgerMenuOpen, setOpenBurgerMenu] = useState(false);
+  const [topValue, setTopValue] = useState("-100px");
+  const [navOpacity, setNavOpacity] = useState("rgba(0, 0, 0, 0.2)");
+
+  const burgerClicked = () => {
+    setOpenBurgerMenu((prevCheck) => !prevCheck);
+
+    if (IsBurgerMenuOpen) {
+      setNavOpacity("rgba(0, 0, 0, 0.2)");
+      document.body.style.overflow = "auto";
+    } else {
+      setNavOpacity("rgba(0, 0, 0, 0)");
+      document.body.style.overflow = "hidden";
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      if (currentScrollPos > prevScrollPos) {
+        console.log("Scrolling Down");
+        setTopValue("-100px");
+      } else {
+        setTopValue("0px");
+        setNavOpacity("rgba(0, 0, 0, 0.2)");
+        console.log("Scrolling Up");
+      }
+
+      if (window.scrollY === 0) {
+        setNavOpacity("rgba(0, 0, 0, 0)");
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
 
   return (
     <>
+      <div className=""></div>
       <div
+        style={{
+          top: topValue,
+          backgroundColor: navOpacity,
+        }}
         className="Navbar flex justify-between items-center p-5
-        bg-transparent text-gray-200 "
+        text-gray-200 sticky z-20 transition-top ease-in-out duration-300"
       >
         <div className="flex items-center">
           <img src="/sweden-flag.png" alt="" className="w-11 h-auto" />
           {/* <div className="ml-2">
             <h2 style={{ fontSize: "10px" }}>Stockholm</h2>
-            <h2 style={{ fontSize: "10px" }}>Sweden</h2>
           </div> */}
         </div>
         <div className="nav-links">
-          <ul className="flex gap-4 text-1xl items-center">
+          <ul className="flex gap-4 text-1xl items-center ">
             <li className="">
               <BsFillMoonStarsFill />
             </li>
-            <li>
-              <button className="git-btn bg-transparent text-gray-200  p-1.5 ml-3 rounded-lg border-2 border-gray-200  ">
+            <li className="flex">
+              {/* <div className=" flex items-center gap-1">
+                <a href="gap-2">Toolkit,</a>
+                <a href="">Projects,</a>
+                <a href="">Experience,</a>
+                <a href="">Contact</a>
+              </div> */}
+              <button className="resume-btn bg-transparent text-gray-200  p-1.5 ml-3 rounded-lg border-2 border-gray-200  ">
                 Resume
               </button>
-              <button className="git-btn  bg-gray-200  text-black p-2 ml-3 rounded-lg ">
-                Git repos
+              <button className="repo-btn bg-gray-200  text-black p-2 ml-3 rounded-lg ">
+                Git repo
               </button>
             </li>
             <li>
-              <BsList style={{ fontSize: "35px" }} />
+              <button
+                onClick={burgerClicked}
+                style={{
+                  fontSize: "35px",
+                  border: "2px solid white",
+                  borderRadius: "10px",
+                  background: "none",
+                  cursor: "pointer",
+                }}
+                className="z-30 relative"
+              >
+                <BsList className="burger-btn" />
+              </button>
             </li>
           </ul>
         </div>
+      </div>
+
+      <div
+        style={{
+          backdropFilter: "blur(10px)",
+          backgroundColor: "rgba(4, 4, 4, 0.9)",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          // height: "100%",
+          height: IsBurgerMenuOpen ? "100%" : "0%",
+          // display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: "10",
+          transition: "height 0.3s ease-in-out",
+          overflow: "hidden",
+        }}
+        className="dropdown-menu"
+      >
+        <ul className="">
+          <li className="flex flex-col font-bold text-white text-3xl gap-10 z-40 mt-32">
+            <button>Toolkit</button>
+            <button>Projects</button>
+            <button>Experience</button>
+            <button>Contact</button>
+          </li>
+        </ul>
       </div>
     </>
   );
